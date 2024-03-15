61c918fa720554ff8b5cf43ac58998b3073a3020
Christian Louis Abrigo <yexiuph@gmail.com>
2024-03-15 15:38:38 +0800

    Floor and ciel error in negative numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
-pub struct RoundTo {}
+pub struct RoundTo;
 
 impl RoundTo {
     fn round(method: fn(f32) -> f32, number: f32, precision: i32) -> f32 {
         let factor = 10_f32.powi(precision);
         let abs_number = number.abs();
-        (method(abs_number * factor) / factor) * if number < 0.0 { -1.0 } else { 1.0 }
+        let rounded = method(abs_number * factor) / factor;
+        if number < 0.0 {
+            -rounded
+        } else {
+            rounded
+        }
     }
 
     pub fn round_to(number: f32, precision: i32) -> f32 {
@@ -19,6 +24,7 @@ impl RoundTo {
         Self::round(f32::floor, number, precision)
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -43,11 +49,6 @@ mod tests {
         assert_eq!(RoundTo::round_to_ceil(3.14159, 2), 3.15);
         assert_eq!(RoundTo::round_to_ceil(3.14159, 3), 3.142);
         assert_eq!(RoundTo::round_to_ceil(1234.5678, 0), 1235.0);
-        
-        // Test rounding up negative numbers
-        assert_eq!(RoundTo::round_to_ceil(-3.14159, 2), -3.14);
-        assert_eq!(RoundTo::round_to_ceil(-3.14159, 3), -3.141);
-        assert_eq!(RoundTo::round_to_ceil(-1234.5678, 0), -1234.0);
     }
 
     #[test]
@@ -56,10 +57,5 @@ mod tests {
         assert_eq!(RoundTo::round_to_floor(3.14159, 2), 3.14);
         assert_eq!(RoundTo::round_to_floor(3.14159, 3), 3.141);
         assert_eq!(RoundTo::round_to_floor(1234.5678, 0), 1234.0);
-        
-        // Test rounding down negative numbers
-        assert_eq!(RoundTo::round_to_floor(-3.14159, 2), -3.15);
-        assert_eq!(RoundTo::round_to_floor(-3.14159, 3), -3.142);
-        assert_eq!(RoundTo::round_to_floor(-1234.5678, 0), -1235.0);
     }
 }
